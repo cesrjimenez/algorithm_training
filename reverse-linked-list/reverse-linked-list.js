6302354788304148000185922ce84eff11acaca3
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2021-06-02 16:45:21 -0700

    Time: 100 ms (6.43%), Space: 40.1 MB (89.88%) - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ 
+ constraints:
+ - The number of nodes in the list is the range [0, 5000].
+ - 5000 <= Node.val <= 5000
+ - time: O(n)
+ - space: O(n)
+ 
+ diagram:
+ - test case #1:
+ 
+   1 -> 2 -> 3 -> 4 -> 5
+        n
+    c  
+p
+          
+- test case #2:
+          
+ <- 1 <- 2        
+            n
+            c
+         p        
+          
+ pcode:
+ - edge case:
+    - if no head return empty array
+ - create previous set to null
+ - create current set to root
+ - while current is not null
+    - create next set to root.next
+    - set current.next to previous
+    - set previous to current
+    - set current to next
+ - return previous
+  
+ */
+var reverseList = function(head) {
+    let previous = null;
+    let current = head;
+    
+    while (current !== null) {
+        let next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+    
+    return previous;
+};
